f06137caf976c8bb4ee68302f6efa5120e4e2735
Noel <36836739+NoelShin@users.noreply.github.com>
2019-03-13 15:01:01 +0900

    Add files via upload

---
+++ MNIST_model.py
@@ -0,0 +1,35 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=2, padding=1)  # 14
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1)  # 7
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1)  # 4
+        self.fc = nn.Linear(in_features=4 * 4 * 256, out_features=10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = x.view(x.shape[0], -1)
+        x = self.fc(x)
+        return x
+
+
+class MLP(nn.Module):
+    def __init__(self):
+        super(MLP, self).__init__()
+        self.linear1 = nn.Linear(in_features=28 * 28, out_features=256)
+        self.linear2 = nn.Linear(in_features=256, out_features=128)
+        self.linear3 = nn.Linear(in_features=128, out_features=64)
+        self.linear4 = nn.Linear(in_features=64, out_features=10)
+
+    def forward(self, x):
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        x = F.relu(self.linear3(x))
+        x = self.linear4(x)
+        return x
---
+++ MNIST_test.py
@@ -0,0 +1,37 @@
+import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision import datasets
+if __name__ == '__main__':
+    MODEL = 'MLP'
+
+    # Construct input pipeline
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=[0.], std=[0.5])])
+    dataset = datasets.MNIST(root='./datasets', train=False, transform=transform, download=True)
+    data_loader = DataLoader(dataset=dataset, batch_size=1, shuffle=False, num_workers=0)
+
+    # Define model
+    if MODEL == 'CNN':
+        from MNIST_model import CNN
+        model = CNN()
+    elif MODEL == 'MLP':
+        from MNIST_model import MLP
+        model = MLP()
+    else:
+        raise NotImplementedError("Invalid model type {}. Choose in [CNN, MLP]".format(MODEL))
+
+    # Load the trained model
+    state_dict = torch.load('MNIST_model_{}.pt'.format(MODEL)).state_dict()
+    model.load_state_dict(state_dict)
+
+    # Test loop
+    total_step = 0
+    nb_correct_answers = 0
+    for i, data in enumerate(data_loader):
+        total_step += 1
+        input_tensor, label = data[0], data[1]
+        input_tensor = input_tensor.view(input_tensor.shape[0], -1) if MODEL == 'MLP' else input_tensor
+        classification_results = model(input_tensor)
+        nb_correct_answers += torch.eq(classification_results.argmax(), label).item()
+    print("Average acc.: {} %.".format(nb_correct_answers / len(data_loader) * 100))
+
