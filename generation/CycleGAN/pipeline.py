796f5e48bb083f1cfb4e9ec9c08ba673fcda71a0
Noel <36836739+NoelShin@users.noreply.github.com>
2019-04-24 14:34:26 +0900

    Update pipeline.py

--- generation/CycleGAN/pipeline.py
+++ generation/CycleGAN/pipeline.py
@@ -7,25 +7,34 @@ from PIL import Image
 
 
 class CustomDataset(Dataset):
-    def __init__(self, opt):
+    def __init__(self, opt, mode):
         super(CustomDataset, self).__init__()
         dataset_name = opt.dataset_name
         dir_datasets = opt.dir_datasets
-        is_train = opt.is_train
 
-        if is_train:
-            dir_input = join(dir_datasets, dataset_name, 'Train', 'Input')
-            dir_real = join(dir_datasets, dataset_name, 'Train', 'Real')
+        if mode == 'train':
+            dir_input = join(dir_datasets, dataset_name, 'Train', 'A')
+            dir_real = join(dir_datasets, dataset_name, 'Train', 'B')
             self.list_paths_input, self.list_paths_real = sorted(os.listdir(dir_input)), sorted(os.listdir(dir_real))
             self.dir_input, self.dir_real = dir_input, dir_real
-        else:
-            dir_input = join(dir_datasets, dataset_name, 'Test', 'Input')
+
+        elif mode == 'val':
+            dir_input = join(dir_datasets, dataset_name, 'Test', 'A')
+            dir_real = join(dir_datasets, dataset_name, 'Test', 'B')
+            self.list_paths_input, self.list_paths_real = sorted(os.listdir(dir_input)), sorted(os.listdir(dir_real))
+            self.dir_input, self.dir_real = dir_input, dir_real
+
+        elif mode == 'test':
+            dir_input = join(dir_datasets, dataset_name, 'Test', 'A')
             self.list_paths_input = sorted(os.listdir(dir_input))
-            self.dir_input, self.dir_real = dir_input, None
+            self.dir_input = dir_input
+
+        else:
+            raise NotImplemented("Invalid mode {}. Choose among 'train', 'val', and 'test'.".format(mode))
 
         self.dataset_name = opt.dataset_name
-        self.is_train = is_train
         self.load_size = opt.load_size
+        self.mode = mode
 
     def __getitem__(self, index):
         transforms = list()
@@ -41,9 +50,9 @@ class CustomDataset(Dataset):
         image_input = Image.open(join(self.dir_input, self.list_paths_input[index])).convert('RGB')
         input = transforms(image_input)
 
-        if self.is_train:
-            image_real = Image.open(join(self.dir_real, self.list_paths_real[random.randint(0, len(self.list_paths_real)
-                                                                                            - 1)])).convert('RGB')
+        if self.mode == 'train' or self.mode == 'val':
+            index_random = random.randint(0, len(self.list_paths_real) - 1)
+            image_real = Image.open(join(self.dir_real, self.list_paths_real[index_random])).convert('RGB')
             real = transforms(image_real)
         else:
             real = 0
